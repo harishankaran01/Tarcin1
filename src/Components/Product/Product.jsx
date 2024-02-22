6d11c6931bff802886c0ebc0ab2041946d48bac0
harishankaran01 <ghari3004@gmail.com>
2024-02-22 16:54:03 +0530

    Design has been changed

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import "/src/Pages/Products.css"
-import spyter from "/src/images/Spyder.png"
-import hand1 from "/src/images/hand1.png"
-import hand2 from "/src/images/hand2.png"
-import robots from "/src/images/robots.png"
+import "/src/Pages/Products.css";
 export default function Product(props) {
   return (
+   
  <div className='product'>
-  <div className='inner-content'>
-    <img src={props.img}  width={"400px"}/>
+  
+  <div className='inner-content' >
+    <div className="inner-img">
+      <img src={props.img}  width={"450px"} height={"350px"}/>
+    </div>
     <div className='product-content'>
     <h1>{props.head}</h1>
     <p> {props.content}</p>
