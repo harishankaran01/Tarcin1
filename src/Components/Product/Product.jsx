c612b56aad97f7c3e393ecdd102b09384e353e48
harishankaran01 <ghari3004@gmail.com>
2024-02-22 11:18:23 +0530

    unwanted files are removed

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import "/src/Pages/Products.css"
-import spyter from "/src/images/Spyder.png"
-import hand1 from "/src/images/hand1.png"
-import hand2 from "/src/images/hand2.png"
-import robots from "/src/images/robots.png"
 export default function Product(props) {
   return (
  <div className='product'>
-  <img src={spyter} width={"100px"}/>
-  <img src={hand1} width={"100px"}/>
-  <img src={hand2} width={"100px"}/>
-  <img src={robots} width={"100px"}/>
+ 
   <div className='inner-content'>
     <img src={props.img}  width={"400px"}/>
     <div className='product-content'>
